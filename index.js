43a278c7489ebf963f80f6506375bb6eeee90359
majdmeaiky <majdmeaiky@gmail.com>
2025-04-22 07:31:07 +0300

    your update message

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+
 app.post('/webhook', (req, res) => {
     console.log("Webhook received:", req.body);
     res.status(200).send("OK");
